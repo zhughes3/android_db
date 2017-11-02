6c7c64788196c7223e7fd063f899b4fc05a5276f
zhughes3 <zhughes3@gmail.com>
2017-11-01 22:29:02 -0400

    SQL returning correct photos

--- app/src/main/java/com/zh/cw4/MainActivity.java
+++ app/src/main/java/com/zh/cw4/MainActivity.java
@@ -5,34 +5,33 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     SQLiteDatabase db;
     Button findButton;
-    TextView result;
-    TextView size;
-    TextView tag;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         createDB();
-        result = (TextView) findViewById(R.id.result);
-        size = (TextView) findViewById(R.id.size);
-        tag = (TextView) findViewById(R.id.tag);
         setContentView(R.layout.activity_main);
     }
 
     protected void createDB() {
         db = this.openOrCreateDatabase("CW4", Context.MODE_PRIVATE, null);
 
+        db.execSQL("DROP TABLE IF EXISTS Photos;");
+        db.execSQL("DROP TABLE IF EXISTS Tags;");
 
         db.execSQL("CREATE TABLE IF NOT EXISTS Photos (" +
                 " id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                " location TEXT NOT NULL," +
+                " location TEXT NOT NULL UNIQUE," +
                 " size INTEGER DEFAULT 0" +
                 ") ;");
 
@@ -64,31 +63,56 @@ public class MainActivity extends AppCompatActivity {
         db.execSQL("INSERT INTO Tags VALUES ( " + id + ", '" + tag + "');");
     }
 
-    private void find() {
-        /**
-         * If tag and size both are entered, query the database to find the images
-         *     that have that exact tag and the exact size mentioned in the text views.
-         * If only a tag is entered, show all images having the specified tag.
-         * If only a size is entered, show all the images having the specified size.
-         */
-        String[] selectionArgs = new String[2];
-        selectionArgs[0] = (String) size.getText();
-        selectionArgs[1] = (String) tag.getText();
-
+    private void addResults(Cursor c, TextView result) {
         StringBuffer sb = new StringBuffer();
-
-        Cursor c = db.rawQuery("SELECT * FROM Photos WHERE size= ? AND tag=?;", selectionArgs);
         try {
             while (c.moveToNext()) {
                 for (int i = 0; i < c.getColumnCount(); i++) {
-                    sb.append(c.getString(i));
+                    sb.append(c.getString(i) + "\t");
                 }
                 sb.append("\n");
             }
         } finally {
             c.close();
         }
-
         result.setText(sb.toString());
+    }
+
+    public void query(View view) {
+        TextView result = (TextView) findViewById(R.id.result);
+        EditText size = (EditText) findViewById(R.id.size);
+        EditText tag = (EditText) findViewById(R.id.tag);
+
+        Editable _size = size.getText();
+        Editable _tag = tag.getText();
+
+        /**
+         * If tag and size both are entered, query the database to find the images
+         *     that have that exact tag and the exact size mentioned in the text views.
+         * If only a tag is entered, show all images having the specified tag.
+         * If only a size is entered, show all the images having the specified size.
+         */
+        if (_size.length() > 0 && _tag.length() > 0) {
+            String[] selectionArgs = new String[2];
+            selectionArgs[0] = size.getText().toString();
+            selectionArgs[1] = tag.getText().toString();
+            Cursor c = db.rawQuery("SELECT location FROM Photos WHERE size= ? AND id IN " +
+                    "(SELECT id FROM Tags WHERE tag=?);", selectionArgs);
+            addResults(c, result);
+        } else if (_size.length() > 0 && _tag.length() == 0) {
+            String[] selectionArgs = new String[1];
+            selectionArgs[0] = size.getText().toString();
+            Cursor c = db.rawQuery("SELECT location FROM Photos where size = ?;", selectionArgs);
+            addResults(c, result);
+        } else if (_size.length() == 0 && _tag.length() > 0) {
+            String[] selectionArgs = new String[1];
+            selectionArgs[0] = tag.getText().toString();
+            Cursor c = db.rawQuery("SELECT location FROM Photos WHERE id IN " +
+                    "(SELECT id FROM Tags WHERE tag=?);", selectionArgs);
+            addResults(c, result);
+        }
+
+        _size.clear();
+        _tag.clear();
     }
 }
